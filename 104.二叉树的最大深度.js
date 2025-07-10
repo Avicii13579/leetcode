975d73ce5030250be108a1e70104ec5b0db858d8
Rowan Lee <18699499130@163.com>
2025-07-10 11:26:34 +0800

    feat：104 获取最大深度处理

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ * 思路：
+ *  深度优先 DFS：递归
+ *  广度优先 BFS：迭代，在寻找最短路径等问题上非常有用，求最小深度时通常首选BFS。
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// 深度优先
+// var maxDepth = function (root) {
+//   if (!root) {
+//     return 0;
+//   }
+
+//   const leftDepth = maxDepth(root.left);
+//   const rightDepth = maxDepth(root.right);
+
+//   return Math.max(leftDepth, rightDepth) + 1;
+// };
+
+// 广度优先
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  // 用数组模拟队列
+  const queue = [root];
+  let depth = 0;
+
+  while (queue.length > 0) {
+    // 当前节点的数量
+    const levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift(); // 节点出队
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    // 完成一层遍历 深度+1
+    depth++;
+  }
+  return depth;
+};
+
+// @lc code=end
